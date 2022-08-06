1b97d7baf5123ade3d574ae7a94537083a5a998b
Danil Liashchenko <mrdenkil777@gmail.com>
2022-08-06 14:55:28 +0300

    refactor code

--- js/app.js
+++ js/app.js
@@ -2,24 +2,35 @@ const cardOfHumans = document.getElementById("allHumans");
 const searchBar = document.getElementById("search");
 const checkboxesGender = document.getElementById("filter_gender");
 
-let arrFiltered;
+//TODO: call displayUsers function just once
+
+let filterFriend;
+
 fetch("https://randomuser.me/api/?results=10")
   .then((response) => response.json())
   .then((users) => {
-    const friends = users.results;
-    displayUsers(friends);
-    sortFriends(friends);
-    selectFriends(friends);
-    filterByGender(friends);
-    searchFriends(friends);
+    renderFriends(users.results);
+    document.addEventListener("click", (event) => filterSetting(event, users.results));
   });
-// function renderFriends () {
 
-// }
+function renderFriends(friends) {
+  selectFriends(friends);
+  searchFriends();
+}
+
+function filterSetting(event, friends) {
+  selectFriends(friends);
+
+  if (event.target.closest(".filter_menu")) sortFriends(event.target, friends);
+}
+
 const displayUsers = (friends) => {
+  //TODO: rename variables
+  //TODO: табуляция для html
   const htmlString = friends
     .map((item) => {
-      return `<div class="card_person">
+      return `
+    <div class="card_person">
     <div class="card_container">
     <img src="${item.picture.large}">
     <h3 class="card_h3">${item.name.first} ${item.name.last}</h3>
@@ -33,84 +44,77 @@ const displayUsers = (friends) => {
 };
 
 function increaseSortByName(friends) {
-  let sortedIncrease = friends.sort(function (a, b) {
+  const sortedIncrease = friends.sort(function (a, b) {
     if (a.name.first.toLowerCase() < b.name.first.toLowerCase()) return -1;
     if (a.name.first.toLowerCase() > b.name.first.toLowerCase()) return 1;
     return 0;
   });
   displayUsers(sortedIncrease);
 }
+
 function decreaseSortByName(friends) {
-  let sortedDecrease = friends.sort(function (a, b) {
+  const sortedDecrease = friends.sort(function (a, b) {
     if (a.name.first.toLowerCase() > b.name.first.toLowerCase()) return -1;
     if (a.name.first.toLowerCase() < b.name.first.toLowerCase()) return 1;
     return 0;
   });
   displayUsers(sortedDecrease);
 }
-function sortFriends(friends) {
-  document.querySelector(".filter_menu").addEventListener("click", (event) => {
-    const button = event.target.id;
-    switch (button) {
-      case "a-z":
-        increaseSortByName(friends);
-        break;
-      case "z-a":
-        decreaseSortByName(friends);
-        break;
-      case "0-9":
-        increaseSortByAge(friends);
-        break;
-      case "9-0":
-        decreaseSortByAge(friends);
-        break;
-    }
-  });
+
+function sortFriends(target, friends) {
+  const button = target.id;
+  switch (button) {
+    case "a-z":
+      increaseSortByName(friends);
+      break;
+    case "z-a":
+      decreaseSortByName(friends);
+      break;
+    case "0-9":
+      increaseSortByAge(friends);
+      break;
+    case "9-0":
+      decreaseSortByAge(friends);
+      break;
+  }
 }
+
 function increaseSortByAge(friends) {
-  let sortedAgeIncrease = friends.sort(function (a, b) {
+  const sortedAgeIncrease = friends.sort(function (a, b) {
     return a.dob.age - b.dob.age;
   });
   displayUsers(sortedAgeIncrease);
 }
+
 function decreaseSortByAge(friends) {
-  let sortedAgeDecrease = friends.sort(function (a, b) {
+  const sortedAgeDecrease = friends.sort(function (a, b) {
     return b.dob.age - a.dob.age;
   });
   displayUsers(sortedAgeDecrease);
 }
+
 function filterByGender(friends) {
-  let checkboxes = Array.from(
-    document.querySelectorAll(".gender_checkbox:checked")
+  const checkboxes = Array.from(document.querySelectorAll(".gender_checkbox:checked"));
+  filterFriend = friends.filter((item) =>
+    checkboxes.some((gender) => item.gender === gender.value)
   );
-  arrFiltered = friends.filter((item) => {
-    return checkboxes.some((gender) => {
-      return item.gender === gender.value;
-    });
-  });
-  console.log(arrFiltered);
-  displayUsers(arrFiltered);
-}
-function selectFriends(friends) {
-  filterByGender(friends);
-  if (arrFiltered.length == 0) {
-    cardOfHumans.innerHTML = "";
-  } else {
-    displayUsers(arrFiltered);
-  }
 }
 
-// SEARCH
+//TODO: rename function
+function selectFriends(friends) {
+  filterByGender(friends);
+  displayUsers(filterFriend);
+}
 
 function searchFriends() {
   searchBar.addEventListener("keyup", (e) => {
-    let searchedFriend = e.target.value.toLowerCase();
-    let filteredFriends = arrFiltered.filter((item) => {
-      return (
-        item.name.first.toLowerCase().includes(searchedFriend) ||
-        item.name.last.toLowerCase().includes(searchedFriend)
-      );
+    const search = e.target.value.toLowerCase();
+
+    const filterFriends = filterFriend.filter((item) => {
+      const { first: firstName, last: lastName } = item.name;
+      return `${firstName} ${lastName}`.toLowerCase().includes(search);
     });
-    displayUsers(filteredFriends);
+
+    displayUsers(filterFriends);
   });
 }
